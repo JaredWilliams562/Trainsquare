f489a0d557f54280966dfa08939f693731990298
Jared Williams <97714133+JaredWilliams562@users.noreply.github.com>
2022-05-25 00:56:31 -0700

    Add files via upload

---
+++ .NET/Venues.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain
+{
+    public class Venue
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int LocationId { get; set; }
+        public BaseLocationMapper Location { get; set; }
+        public string Url { get; set; }
+        public int CreatedBy { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public string ImageUrl { get; set; }
+
+
+    }
+}
